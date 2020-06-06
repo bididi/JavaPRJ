ef35a71b0e6ae73ff3e00115563b08f3001dc7c4
GUEURET Elodie <elodie.gueuret@efrei.net>
2020-06-06 18:13:02 +0200

    début

--- src/Joueur.java
+++ src/Joueur.java
@@ -6,26 +6,31 @@ public class Joueur {
     int score;
     String etat;
 
-
+//Création d'un joueur
     public Joueur(String pseudo){
         this.numJoueur = numJoueur++;
         this.pseudo = pseudo;
         this.score = 0;
         this.etat = " ";
     }
+    //récupération d'une saisi par un joueur
     public String saisir(){
         Scanner input = new Scanner(System.in);
         String réponse = input.next();
         return réponse;
 
     }
+
     public void afficher(){
 
     }
+    // Mise à jour du score d'un joueur
     public void mAJScore(){
-        this.score =
+        this.score =;
 
     }
+
+    // mise à jour de l'état du joueur
     public void changerEtat(){
 
     }
